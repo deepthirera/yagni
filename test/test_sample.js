faf411108a247d6cd24b741b03b44f656390d591
Deepthi <deepthirera@gmail.com>
2016-01-28 23:02:42 +0530

    webpack transpiling

---
+++ test/test_sample.js
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("a", () => {
+   it("should", () => {
+       expect(1).to.equal(1)
+   })
+});
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack/hot/dev-server',
+        'webpack-dev-server/client?http://localhost:8080',
+        path.resolve(__dirname, 'src/index.js')
+    ],
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    }
+};
